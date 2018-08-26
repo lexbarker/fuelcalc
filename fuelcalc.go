8d8d8b09b372be4c22bcc04e91d31d79fcade6d3
Lex Barker <lex1903@gmail.com>
2018-08-26 22:48:28 +0100

    it's late

--- fuelcalc.go
+++ fuelcalc.go
@@ -4,14 +4,32 @@ import "fmt"
 
 //import "fmt"
 
-func main() {
-	miles := 300
-	var litres float64
-	//var litre float64
-	const l2gal = 4.53
-	fmt.Scan(&litres)
-	mpl := float64(miles) / float64(litres)
+type Falc struct {
+	miles  float64
+	litres float64
+	//l2gal  float64
+}
+
+const l2gal = 4.53
+
+func (fc *Falc) Consumption() {
+	mpl := fc.miles / fc.litres
 	fmt.Printf("you achieved %f mile per litre\n", mpl)
 	mpg := mpl * l2gal
 	fmt.Printf("mpg  = %f\n", mpg)
+}
+
+func main() {
+	var fc Falc
+	fc.miles = 300
+	//var litres float64
+	//var litre float64
+
+	fmt.Scan(&fc.litres)
+	fc.Consumption()
+	//mpl := float64(miles) / float64(litres)
+	//fmt.Printf("you achieved %f mile per litre\n", mpl)
+	//mpg := mpl * l2gal
+	//fmt.Printf("mpg  = %f\n", mpg)
+
 }
