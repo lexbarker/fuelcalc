360462b1add2522c015a42a654a371c7ad9e3b45
Lex Barker <lex1903@gmail.com>
2018-08-27 14:10:11 +0100

    split the modules and set as a package 'fuelcalc'. removed main

--- fuelcalc.go
+++ fuelcalc.go
@@ -1,35 +1,14 @@
-package main
+package fuelcalc
 
 import "fmt"
 
 //import "fmt"
 
-type Falc struct {
-	miles  float64
-	litres float64
-	//l2gal  float64
-}
-
-const l2gal = 4.53
-
 func (fc *Falc) Consumption() {
-	mpl := fc.miles / fc.litres
+	mpl := fc.Miles / fc.Litres
 	fmt.Printf("you achieved %f mile per litre\n", mpl)
-	mpg := mpl * l2gal
+	mpg := mpl * L2gal
 	fmt.Printf("mpg  = %f\n", mpg)
 }
 
-func main() {
-	var fc Falc
-	fc.miles = 300
-	//var litres float64
-	//var litre float64
-
-	fmt.Scan(&fc.litres)
-	fc.Consumption()
-	//mpl := float64(miles) / float64(litres)
-	//fmt.Printf("you achieved %f mile per litre\n", mpl)
-	//mpg := mpl * l2gal
-	//fmt.Printf("mpg  = %f\n", mpg)
-
-}
+func Testmeout() {}
---
+++ vars.go
@@ -0,0 +1,9 @@
+package fuelcalc
+
+type Falc struct {
+	Miles  float64
+	Litres float64
+	//l2gal  float64
+}
+
+const L2gal = 4.53
