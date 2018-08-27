4692b24c393434ef68fe6450f3f94beb27de40bb
Lex Barker <lex1903@gmail.com>
2018-08-27 21:33:36 +0100

    refactored the structs

--- vars.go
+++ vars.go
@@ -1,9 +1,20 @@
+// vars.go provides the variables, contstants structs and interfaces for the fuelcalc package
 package fuelcalc
 
-type Falc struct {
+//mpg struct, provides miles and litres to determine MPG
+type Mpg struct {
 	Miles  float64
 	Litres float64
-	//l2gal  float64
 }
 
+//constant for litre to gallon conversion
 const L2gal = 4.53
+
+//Provides Miles and mpg to determine fuel used
+type Fuelused struct {
+	Miles  float64
+	Mpg    float64
+	Litres float64
+}
+
+const Gal2l = 4.54
